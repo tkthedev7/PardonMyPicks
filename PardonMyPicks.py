6c19a636149d94fa5f9a445f4066dc4991b41ae8
Tristan Klinski <tklinski@oxya.com>
2022-02-02 12:07:00 -0500

    Linking VSCode Folder to GitHub Repository

---
+++ PardonMyPicks.py
@@ -0,0 +1,33 @@
+# save this as app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [ 
+    {
+        'author': 'Tristan Klinski',
+        'title': 'Blog Post 1',
+        'content': 'My over is not on the Jets',
+        'date_posted': 'January 31st, 2022'
+    },
+    
+    {
+        'author': 'Tristan Klinski',
+        'title': 'Blog Post 2',
+        'content': 'My under is still not on the Jets',
+        'date_posted': 'February 1st, 2022'
+    }
+]
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
