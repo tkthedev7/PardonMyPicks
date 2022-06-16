ef8558d4b55ee3c464770045174aa1a1c147afb2
Tristan <tklinski@oxya.com>
2022-06-16 09:29:48 -0400

    working on forms

--- PardonMyPicks.py
+++ PardonMyPicks.py
@@ -1,32 +1,56 @@
-# save this as app.py
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
-posts = [ 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+posts = [
     {
-        'author': 'Tristan Klinski',
+        'author': 'Corey Schafer',
         'title': 'Blog Post 1',
-        'content': 'My over is not on the Jets',
-        'date_posted': 'January 31st, 2022'
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Tristan Klinski',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
-        'content': 'My under is still not on the Jets',
-        'date_posted': 'February 1st, 2022'
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
-app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
